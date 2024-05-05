5d06cdc5a89036dc7d8fc8f3f1b6a150a8187283
samuele12345 <samueleriva2016@gmail.com>
2024-05-05 11:48:51 +0200

    richiesta permessi 1

--- app/src/main/java/com/example/progrivazucchi/MainActivity.kt
+++ app/src/main/java/com/example/progrivazucchi/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.progrivazucchi
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
+const val REQUEST_CODE =200
 class MainActivity : AppCompatActivity() {
+    //richiesta dei permessi necessari
+    private var permissions = arrayOf(Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION)
+
+    private var permissionGranted = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        permissionGranted = ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED
+
+        if(!permissionGranted)
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE) //all'utente verra presentata la
+            //interfaccia utente per richiedere i permessi, successivamente verrà informato se sono stati accettati
     }
 }
