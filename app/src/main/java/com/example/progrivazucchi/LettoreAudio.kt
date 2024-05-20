7c6eb1b1b046d5a5972d1527fd0a76f113fa5fec
samuele12345 <samueleriva2016@gmail.com>
2024-05-20 12:42:37 +0200

    creati il layout e la classe lettore audio

---
+++ app/src/main/java/com/example/progrivazucchi/LettoreAudio.kt
@@ -0,0 +1,11 @@
+package com.example.progrivazucchi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LettoreAudio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lettore_audio_)
+    }
+}
